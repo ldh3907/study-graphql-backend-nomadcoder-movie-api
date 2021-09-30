e5995dda418a05b185d53e3f53980d0ecf409a73
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-09-30 23:48:48 +0900

    update : DB, resolvers, schema
    update : DB, resolvers, schema

--- graphql/DB.js
+++ graphql/DB.js
@@ -1,49 +1,9 @@
-let movies = [
-  {
-    id: 0,
-    name: "avengers",
-    score: 5,
-  },
-  {
-    id: 1,
-    name: "star wars",
-    score: 2,
-  },
-  {
-    id: 2,
-    name: "jjang gu",
-    score: 3,
-  },
-  {
-    id: 3,
-    name: "voice",
-    score: 4,
-  },
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanMovies.length) {
-    movies = cleanMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+export const getMovie = (limit, rating) => {
+  fetch(`${API_URL}`)
+    .then(((res) => res.json)())
+    .then((json) => json.data.movies);
 };

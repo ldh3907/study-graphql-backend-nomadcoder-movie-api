9557a49edc1288ee230a17556ccc9dedc8c2b0da
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-10-03 22:20:26 +0900

    update : DB, resolvers, schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
-import { getMovies } from "./DB";
+import { getMovie, getMovies, getSuggestions } from "./DB";
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestion: (_, { id }) => getSuggestions(id),
   },
 };
 
